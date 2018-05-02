a8baf049935791a50d927b41426977f61d85c7bb
Linda <oneill.linda21@gmail.com>
2018-05-02 17:23:47 -0500

    add initialize method

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,1 +1,15 @@
+require 'httparty'
 
+class Kele
+  include HTTParty
+  base_uri "https://www.bloc.io/api/v1"
+
+  def initialize(email, password)
+    response = self.class.post( "/sessions", body: { email: email, password: password } )
+    if response.code == 404
+      raise "Those credentials are invalid. Please try again."
+    else
+      @auth_token = response["auth_token"]
+    end
+  end
+end
