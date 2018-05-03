3a0b1591f7d8668ee492f3e29dcc2378563a2fab
Linda <oneill.linda21@gmail.com>
2018-05-03 18:13:54 -0500

    add get_availability method

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,10 +6,32 @@ class Kele
 
   def initialize(email, password)
     response = self.class.post( "/sessions", body: { email: email, password: password } )
+    #return error for invalid credentials
     if response.code == 404
       raise "Those credentials are invalid. Please try again."
     else
       @auth_token = response["auth_token"]
     end
+  end
+
+  def api_url(endpoint)
+    "https://www.bloc.io/api/v1/#{endpoint}"
+  end
+
+  def get_me
+    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+    @user = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("/mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    #only retrieve open time slots
+    open = []
+    JSON.parse(response.body).each do |time_slot|
+      if time_slot["booked"].nil?
+        open.push(time_slot)
+      end
+    end
+    open
   end
 end
